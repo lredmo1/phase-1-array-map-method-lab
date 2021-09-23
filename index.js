75a9e7757722a4b0c2ed64eafca3c7f9a2baa04c
Lisa <redlisa125@gmail.com>
2021-09-23 07:11:42 -1000

    commit tree 6ace5e10d90e0f454e4af88548066f7fb3de2337

--- index.js
+++ index.js
@@ -11,6 +11,65 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials
+// }
+
+// function titleCased(array) {
+//   let finalArray = array.map(function updatedTutorials(string) {
+//     let splitElement = string.split("");
+//     let innerArrayOfWords = splitElement.map(function (words) {
+//       let firstLetterUpperCase = words[0].toUpperCase();
+//       let restOfWord = words.slice(1);
+//       let upperCaseWord = firstLetterUpperCase + restOfWord;
+//       return upperCaseWord;
+//     })
+//     let finalWord = innerArrayOfWords.join("");
+//     return finalWord;
+//   })
+//   return finalArray;
+// }
+
+
+// function whatever(element) {
+//   let arrayOfWords = element.split(" ");
+//   let innerArray = arrayOfWords.map(function (elements) {
+//     let capitals = elements[0].toUpperCase()
+//     let combined = capitals + elements.slice(1)
+//     // console.log(combined)
+//     return combined;
+//   })
+//   let final = innerArray.join(' ')
+//   // console.log(final)
+//   return [final];
+// }
+
+// let updatedTutorials = tutorials.map(function (string) {
+//   let splitElement = string.split("");
+//   let lowerCaseElement = splitElement.toLowerCase();
+//   let firstLetterUpperCase = lowerCaseElement[0].toUpperCase();
+//   let firstLetterUpperCaseToString = firstLetterUpperCase.toString();
+//   let restOfWord = lowerCaseElement.shift();
+//   let restOfWordToString = restOfWord.toString()
+//   let upperCaseWord = firstLetterUpperCaseToString + restOfWordToString;
+//   return upperCaseWord;
+// }
+// )
+
+// updatedTutorials();
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((string) => {
+    let splitElement = string.split(" ");
+    let individualWords = splitElement.map((words) => {
+      let firstLetterUpperCase = words[0].toUpperCase();
+      let restOfWord = words.slice(1);
+      let upperCaseWord = firstLetterUpperCase + restOfWord;
+      return upperCaseWord;
+    })
+    let finalWord = individualWords.join(" ");
+    return finalWord;
+  })
 }
+
+titleCased(tutorials);
